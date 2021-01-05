4b1ac1a740cfd4c6a7f366468362168c87e06098
HuangJiabao <645246861@qqcom>
2021-01-05 14:02:08 +0800

    pythonGui

---
+++ my01.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from tkinter import messagebox
+
+
+# 黄佳宝
+# 2020/1/5
+#
+root = Tk()
+btn01 = Button(root)
+btn01["text"]="点我就送花"
+#btn01.pack()
+
+
+def songhua(e):  # e事件对象
+    messagebox.showinfo("Message","送你一朵花")
+    print("送你一朵花")
+
+btn01.bind("<Button-1>",songhua)
+root.mainloop()
